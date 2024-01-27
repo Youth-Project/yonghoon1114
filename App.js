7e4feda41293e8a1da7e08420cebdb78544d2108
Yonghoon <kimyonghoon1114@gmail.com>
2024-01-27 09:33:35 +0900

    first upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import ref from "./.expo/screen/ref";
+
+const App = () =>{
+  return(
+    <View>
+      <ref></ref>
+    </View>
+  )
+}
+export default App;
